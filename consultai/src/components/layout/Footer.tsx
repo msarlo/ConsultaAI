d18315ce093dd6d4156e43eece6a7a962cd8a1ef
anabzvmf <anabeatrizzvmf@gmail.com>
2025-12-01 09:28:12 -0300

    adicionando acessibilidade para pcd auditivo

--- consultai/src/components/layout/Footer.tsx
+++ consultai/src/components/layout/Footer.tsx
@@ -9,27 +9,19 @@ export default function Footer() {
         <div className="text-sm mb-6 sm:mb-0 text-center sm:text-left" aria-label="Informações da Prefeitura">
           <h2 className="font-semibold text-white ">Prefeitura de Juiz de Fora</h2>
           <p className="text-gray-200">Av. Brasil, 2001 | Centro - Juiz de Fora/MG - CEP: 36060-010</p>
+          <p className="text-gray-200">
+            Telefone: (32) 3690-7392.| E-mail: <a href="mailto:secretariadesaude@pjf.mg.gov.br ." className="hover:underline">secretariadesaude@pjf.mg.gov.br .</a>
+          </p>
           <p className="text-gray-200 mt-1">
             © {new Date().getFullYear()} Todos os direitos reservados.
           </p>
         </div>
         <div className="flex items-center space-x-4 text-white" aria-label="Redes sociais">
-          <Link href="https://www.facebook.com/JuizdeForaPJF/" target="_blank" rel="noopener noreferrer" aria-label="Facebook da Prefeitura de Juiz de Fora" title="Facebook" className="text-white hover:text-blue-200 transition-colors flex items-center">
+          <Link href="https://linktr.ee/prefeiturajuizdefora" target="_blank" rel="noopener noreferrer" aria-label="Facebook da Prefeitura de Juiz de Fora" title="Facebook" className="text-white hover:text-blue-200 transition-colors flex items-center">
             <FaFacebookF className="w-5 h-5 sm:w-6 sm:h-6" aria-hidden="true" />
-            <span className="sr-only">Facebook</span>
+            <span className="sr-only">Acesse nossas redes</span>
           </Link>
-          <Link href="https://x.com/prefeiturajf" target="_blank" rel="noopener noreferrer" aria-label="X (Twitter) da Prefeitura de Juiz de Fora" title="X (Twitter)" className="text-white hover:text-blue-200 transition-colors flex items-center">
-            <FaTwitter className="w-5 h-5 sm:w-6 sm:h-6" aria-hidden="true" />
-            <span className="sr-only">X (Twitter)</span>
-          </Link>
-          <Link href="https://www.youtube.com/JuizdeForaPJF" target="_blank" rel="noopener noreferrer" aria-label="YouTube da Prefeitura de Juiz de Fora" title="YouTube" className="text-white hover:text-blue-200 transition-colors flex items-center">
-            <FaYoutube className="w-5 h-5 sm:w-6 sm:h-6" aria-hidden="true" />
-            <span className="sr-only">YouTube</span>
-          </Link>
-          <Link href="https://www.instagram.com/prefeiturajuizdefora/" target="_blank" rel="noopener noreferrer" aria-label="Instagram da Prefeitura de Juiz de Fora" title="Instagram" className="text-white hover:text-blue-200 transition-colors flex items-center">
-            <FaInstagram className="w-5 h-5 sm:w-6 sm:h-6" aria-hidden="true" />
-            <span className="sr-only">Instagram</span>
-          </Link>
+         
         </div>
       </div>
     </footer>
